f728153cf8bd52b5edd1c853b36c8342b082397c
tpcollins <tpcollins>
2024-09-20 12:49:11 -0600

    can't get this stupid image to move. switching projects. Will look at again this weekend

--- React Template/.next/static/chunks/webpack.js
+++ React Template/.next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e8138ad1f387e18e"; }
+/******/ 		__webpack_require__.h = function() { return "525ea3344aff52af"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
