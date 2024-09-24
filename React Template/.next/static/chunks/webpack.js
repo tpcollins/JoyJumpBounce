786df06d6ca730cfb5dd714e535401bbfb2d8dbb
tpcollins <tpcollins>
2024-09-23 18:27:04 -0600

    adding inventory page. Images are fuzzy. Need to fix

--- React Template/.next/static/chunks/webpack.js
+++ React Template/.next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "525ea3344aff52af"; }
+/******/ 		__webpack_require__.h = function() { return "123c4c0b2f2621b8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
