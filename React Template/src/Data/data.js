1009fdc83550a3a14cc63cfbbc743cc5aba047b7
tpcollins <tpcollins>
2024-10-21 15:49:11 -0600

    graphics set up for invpg. Need to fix button placement and maybe img sizing? 10-21

--- React Template/src/Data/data.js
+++ React Template/src/Data/data.js
@@ -30,9 +30,9 @@ export const navbarData = {
 // 2. inventory section
 export const inventoryData = {
     inventoryItems: [
-        { src: "assets/images/inflatables/WaveRiderWBG2.png", title: "Wave Rider Waterslide", price: "$285 Single Day",description: "kindergarten", font: "'WaveRider', sans-serif", textImg: "assets/images/InvTextImages/waverider.png" },
-        { src: "assets/images/inflatables/ComboWetSlideWBG.png", title: "17' Dry Slide", price: "$200 Single Day", description: "kindergarten", font: "", textImg: "assets/images/InvTextImages/Tropical Combo.png"},
-        { src: "assets/images/common/sc-gallery-3.jpg", title: "Tropical Combo", price: "$255 Single Day Wet, $175 Single Day Dry", description: "kindergarten", font: "", textImg: "" }
+        { src: "assets/images/inflatables/WaveRiderWBG3.png", title: "Wave Rider Waterslide", price: "$285 Single Day",description: "kindergarten", font: "'WaveRider', sans-serif", textImg: "assets/images/InvTextImages/waverider.png" },
+        { src: "assets/images/inflatables/ComboWetSlideWBG.png", title: "Combo Wet Slide", price: "$200 Single Day", description: "kindergarten", font: "", textImg: "assets/images/InvTextImages/Tropical Combo.png"},
+        { src: "assets/images/inflatables/DrySlideWBG.png", title: "17' Dry Slide", price: "$255 Single Day Wet, $175 Single Day Dry", description: "kindergarten", font: "", textImg: "assets/images/InvTextImages/DrySlide.png" }
     ]
 }
 
