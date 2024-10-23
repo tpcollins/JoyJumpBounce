ec748261670d8b8b5057a6d6e38c84a2b955e99f
tpcollins <tpcollins>
2024-10-23 08:05:40 -0600

    color scheme good on ab us. Starting TODO2 on abus-pg 1-0-23

--- React Template/src/layouts/Footer.js
+++ React Template/src/layouts/Footer.js
@@ -4,133 +4,126 @@ const Footer = () => {
     <footer id="footer-1">
       <div className="footer-content">
           <div className="inner-content">
-              <div className="container">
-                <div className="row">
-                  <div className="col-12">
-                    <div className="widget-footer">
-                      <div className="widget widget-logo">
-                        <div className="logo-bottom" id="logo-footer">
-                          <Link href="/">
-                            <a>
-                              <img
-                                src="assets/images/logo/logofootert.png"
-                                alt="kinco"
-                              />
-                            </a>
-                          </Link>
-                        </div>
-                        <p className="wrap f-mulish">
-                          Sit amet consectetur adipiscing elit sed do eiusmod teminci
-                          idunt ut labore et dolore magna
-                        </p>
-                        <div className="list-contact">
-                          <ul>
-                            <li className="fx">
-                              <span>
-                                <i className="far fa-map-marker-alt" /> 55 Main
-                                Street, New York
-                              </span>
-                            </li>
-                            <li className="fx">
-                              <a href="mailto:hotline@gmail.com">
-                                <i className="far fa-envelope" /> hotline@gmail.com
-                              </a>
-                            </li>
-                            <li className="fx">
-                              <a href="tel:012345678">
-                                <i className="fal fa-phone" /> +012 (345) 678
-                              </a>
-                            </li>
-                          </ul>
-                        </div>
-                      </div>
-                      <div className="widget widget-business">
-                        <div className="inner">
-                          <div className="op-time">
-                            <h4 className="title-widget">opening hours</h4>
-                            <ul>
-                              <li>
-                                <span className="f-mulish">Sunday - Friday</span>
-                              </li>
-                              <li>
-                                <span className="f-mulish">08 am - 05 pm</span>
-                              </li>
-                            </ul>
-                          </div>
-                          <div className="cls-time">
-                            <p>Every Satarday and Govt Holiday</p>
-                            <h4 className="title-widget">closed</h4>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="widget widget-news st-3">
-                        <h4 className="title-widget">recent news</h4>
-                        <ul className="list-news">
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+                  <div className="widget-footer">
+                    <div className="widget widget-logo">
+                      <p className="wrap f-mulish">
+                        Sit amet consectetur adipiscing elit sed do eiusmod teminci
+                        idunt ut labore et dolore magna
+                      </p>
+                      <div className="list-contact">
+                        <ul>
                           <li className="fx">
-                            <img
-                              src="assets/images/thumbnails/widget9.jpg"
-                              alt="Image"
-                              className="feature"
-                            />
-                            <ul className="box-content">
-                              <li>
-                                <h6 className="title">
-                                  <Link href="/blog-grid">
-                                    <a>Useful Code Extened End Developers</a>
-                                  </Link>
-                                </h6>
-                              </li>
-                              <li>
-                                <Link href="/blog-grid">
-                                  <a className="fx meta-news clr-pri-6">
-                                    <i className="far fa-calendar-alt" />
-                                    25 dec 2021
-                                  </a>
-                                </Link>
-                              </li>
-                            </ul>
+                            <span>
+                              <i className="far fa-map-marker-alt" /> Tuscaloosa, AL
+                            </span>
                           </li>
                           <li className="fx">
-                            <img
-                              src="assets/images/thumbnails/widget10.jpg"
-                              alt="Image"
-                              className="feature"
-                            />
-                            <ul className="box-content">
-                              <li>
-                                <h6 className="title">
-                                  <Link href="/blog-grid">
-                                    <a>Useful Code Extened End Developers</a>
-                                  </Link>
-                                </h6>
-                              </li>
-                              <li>
-                                <Link href="/blog-grid">
-                                  <a className="fx meta-news clr-pri-6">
-                                    <i className="far fa-calendar-alt" />
-                                    25 dec 2021
-                                  </a>
-                                </Link>
-                              </li>
-                            </ul>
+                            <a href="mailto:hotline@gmail.com">
+                              <i className="far fa-envelope" /> JoyJumpInflatables@gmail.com
+                            </a>
+                          </li>
+                          <li className="fx">
+                            <a href="tel:012345678">
+                              <i className="fal fa-phone" /> +(205) 861-4553
+                            </a>
                           </li>
                         </ul>
                       </div>
                     </div>
-                  </div>
-                  <div className="col-12">
-                    <div className="footer-bottom jus-ct">
-                      <p className="copy-right">
-                      <p className="copy-right">
-                        Copyright © {new Date().getFullYear()}, Collins Web Design LLC
-                      </p>
-                      </p>
+                    <div className="widget widget-business">
+                      <div className="inner">
+                        <div className="op-time">
+                          <h4 className="title-widget">opening hours</h4>
+                          <ul>
+                            <li>
+                              <span className="f-mulish">Monday - Friday</span>
+                            </li>
+                            <li>
+                              <span className="f-mulish">08 am - 05 pm</span>
+                            </li>
+                          </ul>
+                        </div>
+                        <div className="cls-time">
+                          <p>Every Satarday and Govt Holiday</p>
+                          <h4 className="title-widget">closed</h4>
+                        </div>
+                      </div>
                     </div>
+                    <div className="widget widget-news st-3">
+                      <h4 className="title-widget">Recent Events</h4>
+                      <ul className="list-news">
+                        <li className="fx">
+                          <img
+                            src="assets/images/thumbnails/widget9.jpg"
+                            alt="Image"
+                            className="feature"
+                          />
+                          <ul className="box-content">
+                            <li>
+                              <h6 className="title">
+                                <Link href="/blog-grid">
+                                  <a>Event 1</a>
+                                </Link>
+                              </h6>
+                            </li>
+                            <li>
+                              <Link href="/blog-grid">
+                                <a 
+                                className="fx meta-news clr-pri-6"
+                                style={{color: '#1AB9FF'}}
+                                >
+                                  <i className="far fa-calendar-alt" />
+                                  09/27/2024
+                                </a>
+                              </Link>
+                            </li>
+                          </ul>
+                        </li>
+                        <li className="fx">
+                          <img
+                            src="assets/images/thumbnails/widget10.jpg"
+                            alt="Image"
+                            className="feature"
+                          />
+                          <ul className="box-content">
+                            <li>
+                              <h6 className="title">
+                                <Link href="/blog-grid">
+                                  <a>Event 2</a>
+                                </Link>
+                              </h6>
+                            </li>
+                            <li>
+                              <Link href="/blog-grid">
+                                <a 
+                                className="fx meta-news clr-pri-6"
+                                style={{color: '#1AB9FF'}}
+                                >
+                                  <i className="far fa-calendar-alt" />
+                                  09/28/2024
+                                </a>
+                              </Link>
+                            </li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-12">
+                  <div className="footer-bottom jus-ct">
+                    <p className="copy-right">
+                      Copyright © {new Date().getFullYear()}, Collins Web Design LLC
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+        </div>
     </footer>
   );
 };
