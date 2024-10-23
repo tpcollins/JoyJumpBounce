724b2d56739ab1c970e9558464721fa017d7f25e
tpcollins <tpcollins>
2024-10-23 10:53:36 -0600

    TODO2 finished. Starting TODO3 (Contact page)

--- React Template/pages/aboutus.js
+++ React Template/pages/aboutus.js
@@ -2,8 +2,17 @@ import Layout from "../src/layouts/Layout";
 import Header1 from "../src/layouts/header/Header1";
 import Footer from "../src/layouts/Footer";
 import About from "./about";
+import { useEffect } from "react";
 
 const AboutUs = () => {
+
+  useEffect(() => {
+    document.body.classList.add('abus-body');
+    
+    return () => {
+      document.body.classList.remove('abus-body');
+    };
+  }, []);
 
   return(
     <Layout noFooter noHeader bodyClass={"main"}>
