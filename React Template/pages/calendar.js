2b1afded764f0675fdf009e066b2f11a83606d38
tpcollins <tpcollins00@gmail.com>
2024-11-19 20:11:30 -0700

    TODO-3a finished. Calendar sizing consistent. Starting TODO-3b 11-19

--- React Template/pages/calendar.js
+++ React Template/pages/calendar.js
@@ -1,20 +1,47 @@
+// import Calendar from 'react-calendar';
+
+// const ReactCalendar = ({handleDateClick}) => {
+    
+//     return(
+//     <>
+//         <div className="container">
+//             <div className="calendar-container">
+//                 <div className="row justify-content-center">
+//                     <div className="col-12">
+//                         <Calendar onClickDay={handleDateClick} />
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </>
+//     );
+// };
+
+// export default ReactCalendar;
+
 import Calendar from 'react-calendar';
 
-const ReactCalendar = ({handleDateClick}) => {
-    
-    return(
+const ReactCalendar = ({ handleDateClick }) => {
+  const today = new Date();
+
+  return (
     <>
-        <div className="container">
-            <div className="calendar-container">
-                <div className="row justify-content-center">
-                    <div className="col-12">
-                        <Calendar onClickDay={handleDateClick} />
-                    </div>
-                </div>
+      <div className="container">
+        <div className="calendar-container">
+          <div className="row justify-content-center">
+            <div className="col-12">
+              <Calendar
+                onClickDay={handleDateClick}
+                tileDisabled={({ date, view }) =>
+                  view === 'month' && date < today // Disable dates before today in the month view
+                }
+              />
             </div>
+          </div>
         </div>
+      </div>
     </>
-    );
+  );
 };
 
 export default ReactCalendar;
