a6e3a8304a41a8314ee1ef4b1b41126bb9c90125
tpcollins <tpcollins00@gmail.com>
2024-12-01 20:58:51 -0700

    cart icon disappearing from checkout page and modal collapsing on checkout page 12-1

--- ReactTemplate/pages/cartwrapper.js
+++ ReactTemplate/pages/cartwrapper.js
@@ -2,13 +2,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import ShoppingCart from "../src/R Components/shoppingcart";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CheckoutModal from "../src/R Components/checkoutmodal";
+import { useRouter } from "next/router";
 
 // Wrapper for cart logic
 const CartWrapper = () => {
   const cartItems = useSelector((state) => state.cart.items);
   const cartItemsLength = cartItems.length;
+
+  const router = useRouter();
+  const [hideCart, setHideCart] = useState(false);
 
   const [isOpen, setIsOpen] = useState(false);
 
@@ -18,10 +22,21 @@ const CartWrapper = () => {
     console.log("show on booking: ", isOpen)
   };
 
+  useEffect(() => {
+    const hideOnPages = ['/checkout']; // Pages to hide the button and close the modal
+    const shouldHide = hideOnPages.includes(router.pathname);
+
+    setHideCart(shouldHide); // Hide the ShoppingCart component
+    if (shouldHide) {
+        setIsOpen(false); // Close the modal if on the checkout page
+    }
+  }, [router.pathname]);
+
+
   return (
     <>
       
-      {cartItemsLength > 0 && (
+      {cartItemsLength > 0 && !hideCart && (
         <ShoppingCart 
         onClick={handleModalOpen}
         />
