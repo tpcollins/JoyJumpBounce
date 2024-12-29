4ba1297db238c8b44ed6f680505dac188d137de1
tpcollins <tpcollins00@gmail.com>
2024-12-29 16:31:06 -0700

    socials added to footers. Need to implement on mobile 12-29

--- ReactTemplate/src/layouts/Footer2.js
+++ ReactTemplate/src/layouts/Footer2.js
@@ -77,6 +77,7 @@ const Footer2 = ({footerData}) => {
                       </div>
                     </div>
                   </div>
+
                   <div className="widget widget-news st-3 additional-questions f2">
                     <h4 className="title-widget additional-title">
                       Additional Questions? Get in Contact with us Today!
@@ -100,6 +101,31 @@ const Footer2 = ({footerData}) => {
                   </div>
                 </div>
               </div>
+
+              {/* Social Media Icons */}
+              <div className="social-icons">
+                    <a
+                        href="https://www.facebook.com/people/JoyJump-Inflatables/61556302704028/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="social-link"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="#1877F2" viewBox="0 0 24 24" width="40px" height="40px">
+                            <path d="M24 12c0-6.627-5.373-12-12-12S0 5.373 0 12c0 5.991 4.388 10.954 10.125 11.854V15.47h-3.047v-3.47h3.047v-2.644c0-3.007 1.792-4.668 4.533-4.668 1.312 0 2.686.235 2.686.235v2.953h-1.514c-1.491 0-1.953.928-1.953 1.88v2.244h3.328l-.531 3.47h-2.797v8.384C19.612 22.954 24 17.991 24 12z"/>
+                        </svg>
+                    </a>
+                    <a
+                        href="https://www.instagram.com/joyjump_inflatables/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="social-link"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="#E4405F" viewBox="0 0 24 24" width="40px" height="40px">
+                            <path d="M7.75 2h8.5C19.98 2 22 4.02 22 7.75v8.5C22 19.98 19.98 22 16.25 22h-8.5C4.02 22 2 19.98 2 16.25v-8.5C2 4.02 4.02 2 7.75 2zm-.43 2C5.517 4 4 5.517 4 7.32v9.36C4 18.483 5.517 20 7.32 20h9.36c1.803 0 3.32-1.517 3.32-3.32V7.32C20 5.517 18.483 4 16.68 4H7.32zm9.3 1.14h.72a1.14 1.14 0 1 1 0 2.28h-.72a1.14 1.14 0 1 1 0-2.28zM12 7.6a4.4 4.4 0 1 1 0 8.8 4.4 4.4 0 0 1 0-8.8zm0 1.8a2.6 2.6 0 1 0 0 5.2 2.6 2.6 0 0 0 0-5.2z"/>
+                        </svg>
+                    </a>
+              </div>
+
               <div className="col-12">
                 <div className="footer-bottom f2 jus-ct">
                   <p className="copy-right">
