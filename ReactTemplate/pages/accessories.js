7f691811ab21bf4084bf7a614acb1760cef676ed
tpcollins <tpcollins00@gmail.com>
2024-12-28 18:37:24 -0700

    accessories good for mbl resp. Doing inv page 12-18

--- ReactTemplate/pages/accessories.js
+++ ReactTemplate/pages/accessories.js
@@ -4,6 +4,7 @@ import { accessoryData, footerData } from '../src/Data/data';
 import Footer from '../src/layouts/Footer';
 import { useEffect } from 'react';
 import Header1 from "../src/layouts/header/Header1"; 
+import Layout from '../src/layouts/Layout';
 
 const Accessories = () => {
     useEffect(() => {
@@ -16,54 +17,59 @@ const Accessories = () => {
 
     return (
     <>
+
+        <Layout noFooter noHeader bodyClass={"main"}>
     
-        <Header1 />
+            <Header1 />
 
-        <div className='container'>
+            <div className='container'>
 
-            <div className="accessories-container">
+                <div className="accessories-container">
 
-                <div 
-                className="title-heading st-4"
-                style={{paddingTop: '250px'}}
-                >
-                    <div className="sub-heading clr-pri-3 f-mulish">
-                        <LeftArrow />
-                        <span className="inner-sub st-1">Accessories</span>
-                        <RightArrow />
-                    </div>
-                    {/* <h2 className="title clr-pri-2">
-                        Accessories can be Booked After Selecting Your Desired Float on our Booking Page.
-                    </h2> */}
-                    <h5
-                    className='clr-pri-2'
-                    >Accessories can be Booked After Selecting Your Desired Float on our Booking Page.
-                    </h5>
-                </div>
-
-                <hr className="separator-line" />
-
-                <div className="accessories-grid">
-                    {accessoryData.accessories.map((item, index) => (
-                    <div key={index} className="accessories-item">
-                        <img src={item.imgSrc} alt={item.title} className="accessories-image" />
-                        <div className="accessories-info">
-                        <h2 className="accessories-title">{item.title}</h2>
-                        <p className="accessories-price">{item.showPrice}</p>
+                    <div 
+                    className="title-heading st-4"
+                    style={{paddingTop: '250px'}}
+                    >
+                        <div className="sub-heading clr-pri-3 f-mulish">
+                            <LeftArrow />
+                            <span className="inner-sub st-1">Accessories</span>
+                            <RightArrow />
                         </div>
-
-                        {index < accessoryData.length - 1 && <hr className="separator-line" />}
+                        {/* <h2 className="title clr-pri-2">
+                            Accessories can be Booked After Selecting Your Desired Float on our Booking Page.
+                        </h2> */}
+                        <h5
+                        className='clr-pri-2'
+                        >Accessories can be Booked After Selecting Your Desired Float on our Booking Page.
+                        </h5>
                     </div>
-                    ))}
+
+                    
+
+                    <div className="accessories-grid">
+                        <hr className="separator-line" />
+                        {accessoryData.accessories.map((item, index) => (
+                        <div key={index} className="accessories-item">
+                            <img src={item.imgSrc} alt={item.title} className="accessories-image" />
+                            <div className="accessories-info">
+                            <h2 className="accessories-title">{item.title}</h2>
+                            <p className="accessories-price">{item.showPrice}</p>
+                            </div>
+
+                            {index < accessoryData.length - 1 && <hr className="separator-line" />}
+                        </div>
+                        ))}
+                    </div>
+                    
+                    <hr className="separator-line" />
+
                 </div>
-                
-                <hr className="separator-line" />
-
             </div>
-        </div>
 
-        <Footer footerData={footerData}/>
-    </>
+            <Footer footerData={footerData}/>
+
+            </Layout>
+        </>
     );
 };
 
