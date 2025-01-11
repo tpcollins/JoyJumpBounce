029d0a50438e5d5ec08769323dd397e333feed41
tpcollins <tpcollins00@gmail.com>
2025-01-10 22:14:22 -0700

    linting

--- ReactTemplate/pages/_app.js
+++ ReactTemplate/pages/_app.js
@@ -38,10 +38,6 @@ const App = ({ Component, pageProps }) => {
                 <link 
                 href="https://fonts.googleapis.com/css2?family=Salsa&display=swap"
                 rel="stylesheet" />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Salsa&display=swap"
-                    rel="stylesheet"
-                />
 
 
                 {/* Meta for Web App */}
@@ -49,7 +45,7 @@ const App = ({ Component, pageProps }) => {
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="mobile-web-app-capable" content="yes" />
 
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
             </Head>
 
 
