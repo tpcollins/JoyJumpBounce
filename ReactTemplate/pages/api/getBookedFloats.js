cc60a203c14028f8250b8302e5751ea063046213
tpcollins <tpcollins00@gmail.com>
2025-01-15 22:49:03 -0700

    accessories excluded from bookedFloats APIs

--- ReactTemplate/pages/api/getBookedFloats.js
+++ ReactTemplate/pages/api/getBookedFloats.js
@@ -1,5 +1,27 @@
-// pages/api/getBookedFloats.js
+// // pages/api/getBookedFloats.js
 import Airtable from 'airtable';
+
+// export default async function handler(req, res) {
+//   if (req.method !== 'POST') {
+//     return res.status(405).json({ message: 'Method not allowed' });
+//   }
+
+//   const { selectedDate } = req.body;
+//   const base = new Airtable({ apiKey: process.env.AIRTABLE_PERSONAL_ACCESS_TOKEN }).base(process.env.AIRTABLE_BASE_ID);
+
+//   try {
+//     const records = await base('Bookings').select({
+//       filterByFormula: `IS_SAME({Booking Date}, '${selectedDate}', 'day')`,
+//     }).firstPage();
+
+//     const bookedFloats = records.map(record => record.fields['Item']);
+//     res.status(200).json({ bookedFloats });
+//   } catch (error) {
+//     console.error('Error querying Airtable:', error);
+//     res.status(500).json({ message: 'Failed to retrieve booked floats', error });
+//   }
+// }
+
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
@@ -11,7 +33,17 @@ export default async function handler(req, res) {
 
   try {
     const records = await base('Bookings').select({
-      filterByFormula: `IS_SAME({Booking Date}, '${selectedDate}', 'day')`,
+      filterByFormula: `
+        AND(
+          IS_SAME({Booking Date}, '${selectedDate}', 'day'),
+          NOT(OR(
+            {Item} = 'Generator',
+            {Item} = 'Inflatable Tent',
+            {Item} = 'Table',
+            {Item} = 'Chair'
+          ))
+        )
+      `,
     }).firstPage();
 
     const bookedFloats = records.map(record => record.fields['Item']);
