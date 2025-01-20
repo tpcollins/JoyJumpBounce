935bc5419148959ae6d4f96e231bdaac240da0a7
Tristan Collins <tristancollins@timothys-MacBook-Pro.local>
2025-01-20 14:49:02 -0700

    fixing medium size screens

--- ReactTemplate/pages/checkout-success.js
+++ ReactTemplate/pages/checkout-success.js
@@ -36,23 +36,38 @@ const Index = () => {
   //   }
   // }, []);
 
-  useEffect(() => {
-    const isOpera = () => {
-      return (
-        !!window.opr ||  // Detects Opera on Desktop
-        !!window.opr?.addons || // Extra check for Opera
-        navigator.userAgent.includes('Opera') || // Older versions
-        (navigator.vendor === 'Google Inc.' && /Mobile/.test(navigator.userAgent) && !/Edg/.test(navigator.userAgent))
-      );
-    };
+  // useEffect(() => {
+  //   const isOpera = () => {
+  //     return (
+  //       !!window.opr ||  // Detects Opera on Desktop
+  //       !!window.opr?.addons || // Extra check for Opera
+  //       navigator.userAgent.includes('Opera') || // Older versions
+  //       (navigator.vendor === 'Google Inc.' && /Mobile/.test(navigator.userAgent) && !/Edg/.test(navigator.userAgent))
+  //     );
+  //   };
 
-    if (isOpera()) {
-      document.body.classList.add('opera-browser');
-      console.log('Opera detected');
-    } else {
-      console.log('Not Opera');
-    }
-  }, []);
+  //   if (isOpera()) {
+  //     document.body.classList.add('opera-browser');
+  //     console.log('Opera detected');
+  //   } else {
+  //     console.log('Not Opera');
+  //   }
+  // }, []);
+
+    // Opera Detection
+    useEffect(() => {
+      const isOpera = () => {
+        const userAgent = navigator.userAgent;
+        return /OPR/.test(userAgent) || /Opera/.test(userAgent); // Detects Opera
+      };
+  
+      if (isOpera()) {
+        document.body.classList.add("opera-browser");
+        console.log("Opera detected");
+      } else {
+        console.log("Not Opera");
+      }
+    }, []);
 
   const isSafari = () => {
     const userAgent = navigator.userAgent;
