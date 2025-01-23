9986987633c2dab8e6a5c05c5282a4a1e519c706
tpcollins <tpcollins00@gmail.com>
2025-01-22 19:39:47 -0700

    added price to new inv

--- ReactTemplate/src/Data/data.js
+++ ReactTemplate/src/Data/data.js
@@ -43,7 +43,7 @@ export const inventoryData = {
         { src: "assets/images/inflatables/WaveRiderWBG2.png", title: "Wave Rider Waterslide", price: "$285 Single Day",description: "kindergarten", font: "'WaveRider', sans-serif", textImg: "assets/images/InvTextImages/waverider.png", pgSection: 'waverider' },
         { src: "assets/images/inflatables/ComboWetSlideWBG.png", title: "Combo Wet Slide", price: "$200 Single Day", description: "kindergarten", font: "", textImg: "assets/images/InvTextImages/Tropical Combo.png", pgSection: 'tropcombo'},
         { src: "assets/images/inflatables/DrySlideWBG.png", title: "17' Dry Slide", price: "$255 Single Day Wet, $175 Single Day Dry", description: "kindergarten", font: "", textImg: "assets/images/InvTextImages/DrySlide.png", pgSection: 'dryslide' },
-        { src: "assets/images/inflatables/ObstacleCourseWBG.png", title: "30 Foot Obstacle Course", price: "$225 Single Day Dry", description: "kindergarten", font: "", textImg: "assets/images/InvTextImages/OCInvText.png", pgSection: 'obc' },
+        { src: "assets/images/inflatables/ObstacleCourseWBG.png", title: "30 Foot Obstacle Course", price: "$225 Single Day Dry", description: "kindergarten", font: "", textImg: "assets/images/InvTextImages/OCInvText2.png", pgSection: 'obc' },
     ]
 }
 
