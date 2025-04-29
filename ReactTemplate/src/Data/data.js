106ca818d52ae5871385f18858e21f24d2a1bee4
tpcollins <tpcollins00@gmail.com>
2025-04-28 18:57:18 -0600

    date going to airtable extra row properly. Pushing to main

--- ReactTemplate/src/Data/data.js
+++ ReactTemplate/src/Data/data.js
@@ -181,7 +181,7 @@ export const bcyHseStockData = {
         id: 1,
         imgSrc: "assets/images/inflatables/WaveRiderResized.png",
         title: "Wave Rider",
-        price: 1,
+        price: 285,
         showPrice: "$285",
         pgSection: "waverider",
       },
