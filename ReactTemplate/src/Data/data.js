43f29c87a4ec01ec3128c6bf83108cc3b8818330
Tristan Collins <tpcollins00@gmail.com>
2025-05-21 17:06:22 -0600

    approval recieved. testing pmt sys

--- ReactTemplate/src/Data/data.js
+++ ReactTemplate/src/Data/data.js
@@ -41,7 +41,7 @@ export const navbarData = {
 export const inventoryData = {
     inventoryItems: [
         { src: "assets/images/inflatables/WaveRiderWBG2.png", title: "Wave Rider Waterslide", price: "$285 Single Day",description: "kindergarten", font: "'WaveRider', sans-serif", textImg: "assets/images/InvTextImages/waverider.png", pgSection: 'waverider' },
-        { src: "assets/images/inflatables/16'SlideUpscaled.png", title: "Palm Plunge", price: "$225 Single Day Dry", description: "kindergarten", font: "", textImg: "assets/images/InvTextImages/PalmPlungeTxtImg.png", pgSection: 'sis' },
+        { src: "assets/images/inflatables/16'SlideUpscaled.png", title: "Palm Plunge", price: "$285 Single Day", description: "kindergarten", font: "", textImg: "assets/images/InvTextImages/PalmPlungeTxtImg.png", pgSection: 'sis' },
         { src: "assets/images/inflatables/ComboWetSlideWBG.png", title: "Combo Wet Slide", price: "$200 Single Day", description: "kindergarten", font: "", textImg: "assets/images/InvTextImages/Tropical Combo.png", pgSection: 'tropcombo'},
         // { src: "assets/images/inflatables/DrySlideWBG.png", title: "17' Dry Slide", price: "$255 Single Day Wet, $175 Single Day Dry", description: "kindergarten", font: "", textImg: "assets/images/InvTextImages/DrySlide.png", pgSection: 'dryslide' },
         { src: "assets/images/inflatables/ObstacleCourseWBG.png", title: "30 Foot Obstacle Course", price: "$225 Single Day Dry", description: "kindergarten", font: "", textImg: "assets/images/InvTextImages/OCInvText2.png", pgSection: 'obc' }
@@ -181,7 +181,7 @@ export const bcyHseStockData = {
         id: 1,
         imgSrc: "assets/images/inflatables/WaveRiderResized.png",
         title: "Wave Rider",
-        price: 285,
+        price: 1,
         showPrice: "$285",
         pgSection: "waverider",
       },
