fbf7574a224c48605d6c65e5989827ce73ff1e4a
tpcollins <tpcollins00@gmail.com>
2025-05-23 16:27:00 -0600

    testing receipt system

--- ReactTemplate/src/Data/data.js
+++ ReactTemplate/src/Data/data.js
@@ -181,7 +181,7 @@ export const bcyHseStockData = {
         id: 1,
         imgSrc: "assets/images/inflatables/WaveRiderResized.png",
         title: "Wave Rider",
-        price: 285,
+        price: 1,
         showPrice: "$285",
         pgSection: "waverider",
       },
